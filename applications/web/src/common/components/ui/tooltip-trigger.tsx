7ca34b08ee9c3c4585954b87282dd7c386ea7174
Tim Hazlett <timhazlett2016@gmail.com>
2026-04-16 10:28:40 -0500

    removing old code

--- applications/web/src/common/components/ui/tooltip-trigger.tsx
+++
@@ -1,7 +0,0 @@
-import type { TooltipTriggerComponentProps as RACTooltipTriggerComponentProps } from 'react-aria-components';
-
-import { TooltipTrigger as RACTooltipTrigger } from 'react-aria-components';
-
-export type TooltipTriggerProps = RACTooltipTriggerComponentProps;
-
-export const TooltipTrigger = RACTooltipTrigger;
--- applications/web/src/routes/home.tsx
+++ applications/web/src/routes/home.tsx
@@ -1,8 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { NumberField } from '#src/common/components/ui/number-field';
-import { TextField } from '#src/common/components/ui/text-field';
-
 export const Route = createFileRoute('/home')({
   component: RouteComponent,
 });
@@ -10,10 +7,7 @@ export const Route = createFileRoute('/home')({
 function RouteComponent() {
   return (
     <div className="flex flex-col gap-10 p-8">
-      <div className="flex flex-col gap-3 max-w-xs">
-        <TextField label="Test Text Field" />
-        <NumberField label="Test Number Field" />
-      </div>
+      <div className="flex flex-col gap-3 max-w-xs">HOME</div>
     </div>
   );
 }
